c7acd62f61e8f0df11e91a748f9086c74aaa8b69
getNurbie <nicolemartins22@hotmail.com>
2018-01-11 10:36:37 +0000

    first commit

---
+++ DataAccess.php
@@ -0,0 +1,67 @@
+<?php
+
+class DataAccess{
+    private $connection;	
+    
+    function connect(){
+        $bd= "sportin5_tpsib17-p4";
+        //$user = "sportin5_tpsib-4";
+		$user = "root";
+        $pwd = "";
+        $server = "localhost";
+		
+        $this->connection = mysqli_connect($server, $user, $pwd, $bd);
+        
+        //verificar se a conexão está bem aberta        
+        if( mysqli_select_db($this->connection, $bd) == false){
+            //erro
+            die("Não conseguiu ligar-se à base de dados".mysqli_error($this->connection));            
+			
+        }else{
+			
+            mysqli_query($this->connection, "set names 'utf8'");
+            mysqli_query($this->connection, "set character_set_connection=utf8");
+            mysqli_query($this->connection, "set character_set_client=utf8");
+            mysqli_query($this->connection, "set character_set_results=utf8");
+        }        
+    }
+    
+    function execute($query){
+        $res = mysqli_query($this->connection, $query);
+        if(!$res){
+            die("Comando inválido".mysqli_error($this->connection));
+        }else
+            return $res;
+    }
+    
+    function disconnect(){
+		mysqli_close($this->connection);
+    }
+	
+	function getGuessWho(){
+		$this->connect();
+		$sql = "select * from guesswho";
+		$result = mysqli_query($this->connection, $sql);
+        if (mysqli_num_rows($result) > 0) {
+            $res= mysqli_fetch_array($result);
+        }
+		$this->disconnect();
+		return $res;
+	}
+	function getGuessWhoAnswer(){
+		$query = "select guessWhoAnswer from guesswho";
+		$this->connect();
+        $res = $this->execute($query);
+		$this->disconnect();
+		return $res;
+	}
+	
+	function getCompleteSentece($id){
+		$query = "select * from completesentece where id = '$id'";
+		$this->connect();
+        $res = $this->execute($query);
+		$this->disconnect();
+		return $res;
+	}
+}
+?>
---
+++ games.php
@@ -0,0 +1,120 @@
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width">
+
+		<title>Games</title>
+
+		<link href="css/style.css" rel="stylesheet" type="text/css">
+	</head>
+
+	<body onLoad="score()">
+		<header>
+			<!-- Menu -->
+			<div class="container">
+				<div id="logo">
+					<h1><span class="destaque">The Gifted</span></h1>
+				</div>
+				<nav>
+					<ul>
+						<li><a href="index.html">Home</a></li>
+						<li><a href="about.html">About</a></li>
+						<li><a href="characters.html">Characters</a></li>
+						<li><a href="#">Episodes</a></li>
+						<li class="selecionado"><a href="#">Games</a></li>
+					</ul>
+				</nav>
+			</div>
+		</header>
+		<section id="audio">
+			<div class="container">
+				<!--<div>Iteration: <span id="iteration"></span></div>
+					<audio controls id="myAudio">
+					  <source src="audio/teste.mp3" type="audio/mpeg">
+					Your browser does not support the audio element.
+					</audio>
+					<input type="text" name="guessWhoAnswer" id="guessWhoAnswer"><input type="button" onClick="verifyAnswer()" value="Ok">
+
+				<div>Time left:</div>
+				<div id="count"></div>-->
+			
+			<?php
+				include_once('DataAccess.php');
+				$da = new DataAccess();
+				$res = $da->getGuessWho();
+				$row = mysqli_fetch_object($res);
+			/*$res1 = $da->getGuessWhoAnswer();
+			$guessWhoAnswer = $_POST['guessWhoAnswer'];
+			if (mysqli_num_rows($res1) > 0) {
+			$row1 = mysqli_fetch_object($res1);
+				
+			}*/
+			echo 
+				"<audio controls id='myAudio'>
+					  <source src='$res[guessWhoPath]' type='audio/mpeg'>
+					</audio>";
+					/*<input type='text' name='guessWhoAnswer' id='guessWhoAnswer'><input type='button' onClick='verifyAnswer()' value='Ok'>*/
+			?>
+			</div>
+			<script>
+				var score;
+				function score(){
+					score = 100;
+				}
+				function verifyAnswer(){
+					var value;
+					var audioAnswer = document.getElementById('guessWhoAnswer');
+					alert(audioAnswer.value);
+					var audio = 'beck';
+					if(audioAnswer.value === ''){
+						alert('You need to answer!');
+					}
+					else if(audioAnswer.value === audio){
+						alert('Right Answer!');
+						alert(score);
+					}
+					else{
+						alert('Wrong Answer');
+						value = score - 10;
+						alert(value);
+						
+					}
+				}
+				var iterations = 1;
+
+                document.getElementById('iteration').innerText = iterations;
+				
+                myAudio.addEventListener('ended', function () {
+					setTimeout(function(){
+						audio.pause();
+						alert("Audio Stop Successfully");
+					},
+					5000);
+                    if (iterations < countdown) {
+
+                        this.currentTime = 0;
+                        iterations ++;
+
+                        document.getElementById('iteration').innerText = iterations;
+
+                    }
+                }, false);
+				
+				let countdown = 15;
+
+				document.getElementById("myAudio").play();
+
+				let interval = setInterval(function() {
+				  countdown -= 1;
+				  document.getElementById("count").innerHTML = countdown;
+				  if (countdown <= 0) {
+					document.getElementById("myAudio").pause();
+					clearInterval(interval);
+				  }
+				}, 1000);
+				
+			</script>
+		</section>
+		
+	</body>
+</html>
